6c7af98633dbba148e11ffc8405623cb7b543b3f
qiuf <qiuf@lafayette.edu>
2018-11-14 23:18:57 -0500

    snake game

--- Final_MainControl.X/snake.c
+++ Final_MainControl.X/snake.c
@@ -1,1 +1,24 @@
 #include "snake.h"
+
+void snakeInit(){
+	setSnakeArray(BODY, 0, 0, 0);
+	setSnakeArray(BODY, 0, 1, 0);
+	setSnakeArray(BODY, 0, 2, 0);
+}
+
+void setSnakeArray(uint8_t type, uint8_t levIndex, uint8_t rowIndex, uint8_t colIndex){
+	snakeArray[levIndex][rowIndex][colIndex] = type;
+}
+
+void move(uint8_t dir){
+	if(!isBorder(getHeadLev(), getHeadRow(), getHeadCol(), dir)){
+		switch(dir){
+			case 2:
+				break; 
+		}
+	}
+	else{
+		
+	}
+}
+
--- Final_MainControl.X/snake.h
+++ Final_MainControl.X/snake.h
@@ -6,7 +6,33 @@
 #include <p32xxxx.h>
 #include "led_control.h"
 
+#define HEAD 1
+#define BODY 2
+#define FOOD 3
+
+struct point{
+	uint8_t lev;
+	uint8_t row;
+	uint8_t col;
+	uint8_t type;
+} pt;
+
+void snakeInit();
+void generateFood();
+void move(uint8_t dir); //2=>back 8=>forward 4=>left 6=>right 1=>down 7=>up   
+void eat();
+void refresh();
+uint8_t isBorder(uint8_t levIndex, uint8_t rowIndex, uint8_t colIndex, uint8_t dir);
+uint8_t isBody(uint8_t levIndex, uint8_t rowIndex, uint8_t colIndex, uint8_t dir);
+void setSnakeArray(uint8_t type, uint8_t levIndex, uint8_t rowIndex, uint8_t colIndex);
+uint8_t getHeadLev();
+uint8_t getHeadRow();
+uint8_t getHeadCol();
+uint8_t random();
+
 uint8_t snakeLength;
+uint8_t snakeArray[LEV_NUM][ROW_NUM][COL_NUM]; //0 => empty, 1 => snake head, 2 => body, 3 => food
+
 
 #endif	/* SNAKE_H */
 
