e60103cc4edf58e4de3fdcdee84b9be873613bba
qiuf <qiuf@lafayette.edu>
2018-12-17 18:34:10 -0500

    Comment letters_display.c

--- Final_MainControl.X/letters_display.c
+++ Final_MainControl.X/letters_display.c
@@ -1,18 +1,20 @@
 #include "letters_display.h"
 
-
+//interrupt timer for scrolling effect 
 volatile uint32_t timer_ms_count;
 void __ISR(0, ipl1auto) InterruptHandler(void){
     timer_ms_count++;
     mT1ClearIntFlag();
 }
 
-
 void runLettersDisplay(){
     ledInit();
     lettersDisplayInit();
+    //Change display text here!
+    //Please also change DISPLAY_CHAR_NUM in the header
     decodeArray("ECE414");
     while(1){
+        //check if the function is changed through the touchscreen
         uint8_t input = decodeFromInputControl();
         if(input == 1)
             break;
@@ -21,6 +23,10 @@ void runLettersDisplay(){
     }
 }
 
+/**
+ * Initialization. Initialize display array and also setup the timer 
+ * 
+ */ 
 void lettersDisplayInit(){
     int i = 0;
     for(i = 0; i < DISPLAY_ARRAY_ROW_NUM; i++){
@@ -39,7 +45,11 @@ void lettersDisplayInit(){
     isSnake = 0;
 }
 
+/**
+ * Decode a single character
+ */ 
 void decode(char c){
+    //add LEV_NUM lines before everything since we want texts to start scrolling from the right
     uint8_t line = arrayCharIndex * 6 + LEV_NUM;
     arrayCharIndex ++;
     switch(c){
@@ -347,6 +357,9 @@ void decode(char c){
     }
 }
 
+/**
+ * Decode a string. Write all the data to the display array
+ */ 
 void decodeArray(char c[]){
     int i = 0;
     for(i = 0; i < DISPLAY_CHAR_NUM; i++){
@@ -354,6 +367,11 @@ void decodeArray(char c[]){
     }
 }
 
+/**
+ * Setup LED mapping. 
+ * If SCROLL_SEC = 0, no scrolling is applied
+ * Else, increment scrollingIndex for mapping
+ */ 
 void setLED(){
     if(SCROLL_MSEC == 0){
         ledMapping(0);
@@ -365,6 +383,7 @@ void setLED(){
         else{
             timer_ms_count = 0;
             ledMapping(scrollIndex);
+            //reset scrollingIndex when reaches the end
             if(scrollIndex == DISPLAY_ARRAY_ROW_NUM - 1) 
                 scrollIndex = 0;
             else
@@ -374,11 +393,16 @@ void setLED(){
     }
 }
 
+/**
+ * Read data from display array and send to cuboid array in the driver
+ */ 
 void ledMapping(uint8_t scrollIndex){
     uint8_t index = 0;
     for(index = 0; index < LEV_NUM; index++){
+        //if out of bound, goes back
         if(index + scrollIndex >= DISPLAY_ARRAY_ROW_NUM){
             uint8_t thisRow = displayArray[index + scrollIndex - DISPLAY_ARRAY_ROW_NUM];
+            //set to last three rows
             setArray(thisRow, index, 5);
             setArray(thisRow, index, 6);
             setArray(thisRow, index, 7);
